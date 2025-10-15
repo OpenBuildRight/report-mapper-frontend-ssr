60123a5ff74c0323ea6fb263660566c280692074
DustinMoriarty <dustin.moriarty@protonmail.com>
2025-10-14 20:25:31 -0500

    Added files to CONTRIBUTING.md

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -5,21 +5,33 @@
 ### Dependencies
 
 #### Node
+
 You will first need the right node version installed. The easiest
-way to manage node is with [nvm](https://www.nvmnode.com/). You will 
+way to manage node is with [nvm](https://www.nvmnode.com/). You will
 then need to enable corepack.
+
 ```shell
 corepack enable
 ```
 
 #### Docker
-We will use [docker](https://www.docker.com/) for build. 
+
+We will use [docker](https://www.docker.com/) for build.
 
 #### Docker Compose
-You will use [docker compose](https://docs.docker.com/compose/install) compose 
+
+You will use [docker compose](https://docs.docker.com/compose/install) compose
 for running our local environment services.
 
+#### Terraform
+
+You will need [terraform](https://developer.hashicorp.com/terraform) for configuring
+the local environment. The best way to manage terraform environments is with
+[tfswitch](https://tfswitch.warrensbox.com/). You will need to make sure that the terraform version matches the
+version in [local-env-setup/main.tf](local-env-setup/main.tf).
+
 ### Install Application
+
 We will use [pnpm](https://pnpm.io/) for package management.
 
 ```shell
@@ -27,5 +39,22 @@ pnpm install
 ```
 
 ### Start Local Services
+There is a local environment that is used to create services for the application to connect to during development.
+This environment uses plain text secrets and is not secure for deployment.
 
+The local environment setup will require that docker and docker compose are installed. This is tested on linux. It
+should work on other operating systems, but it is not tested.
 
+```shell
+pnpm setup-dev
+```
+
+However, managing the local environment does require some awareness of how the local setup works. You may need to
+occasionally use terraform and docker compose to manually intervene.
+
+The [local-env-setup/compose.yaml] is used to start the docker services using docker comose.
+The [local-env-setup/main.tf]
+file is used to configure the keycloak service after it starts up.
+
+The setup script [scripts/setup-local-env.ts](scripts/setup-local-env.ts) also writes envrionment variables to
+[.env.local](.env.local) which coorespond to the settings in the docker containers.
--- local-env-setup/main.tf
+++ local-env-setup/main.tf
@@ -1,17 +1,18 @@
 terraform {
+  required_versions = "~> 1.12.0"
   required_providers {
     keycloak = {
-      source = "keycloak/keycloak"
+      source  = "keycloak/keycloak"
       version = "~> 4.0"
     }
   }
 }
 
 provider "keycloak" {
-        client_id = "admin-cli"
-        username = "kc_admin_user"
-        password = "kc_admin_password"
-        url = "http://localhost:9003"
+  client_id = "admin-cli"
+  username  = "kc_admin_user"
+  password  = "kc_admin_password"
+  url       = "http://localhost:9003"
 }
 
 resource "keycloak_realm" "this" {
@@ -20,14 +21,14 @@ resource "keycloak_realm" "this" {
 }
 
 resource "keycloak_openid_client" "openid_client" {
-  realm_id            = keycloak_realm.this.id
-  client_id           = "test-client"
+  realm_id  = keycloak_realm.this.id
+  client_id = "test-client"
 
-  name                = "test client"
-  enabled             = true
+  name    = "test client"
+  enabled = true
 
-  access_type         = "CONFIDENTIAL"
-  client_secret       = "local-dev-secret-change-in-production"
+  access_type   = "CONFIDENTIAL"
+  client_secret = "local-dev-secret-change-in-production"
 
   valid_redirect_uris = [
     "http://localhost:3000/api/auth/callback/keycloak",
@@ -45,9 +46,9 @@ resource "keycloak_openid_client" "openid_client" {
     "http://127.0.0.1:3001",
   ]
 
-  login_theme = "keycloak"
-  standard_flow_enabled = true
-  implicit_flow_enabled = false
+  login_theme                  = "keycloak"
+  standard_flow_enabled        = true
+  implicit_flow_enabled        = false
   direct_access_grants_enabled = false
 
   extra_config = {
@@ -57,9 +58,9 @@ resource "keycloak_openid_client" "openid_client" {
 }
 
 resource "keycloak_user" "user_with_initial_password" {
-  realm_id   = keycloak_realm.this.id
-  username   = "alice"
-  enabled    = true
+  realm_id = keycloak_realm.this.id
+  username = "alice"
+  enabled  = true
 
   email      = "alice@domain.com"
   first_name = "Alice"
@@ -67,47 +68,47 @@ resource "keycloak_user" "user_with_initial_password" {
 
 
   initial_password {
-    value     = "alice_password"
+    value = "alice_password"
   }
 }
 
 # Admin user (machine user with all permissions)
 resource "keycloak_user" "admin_user" {
-  realm_id   = keycloak_realm.this.id
-  username   = "admin"
-  enabled    = true
+  realm_id = keycloak_realm.this.id
+  username = "admin"
+  enabled  = true
 
   email      = "admin@example.com"
   first_name = "Admin"
   last_name  = "User"
 
   initial_password {
-    value     = "admin_password"
+    value = "admin_password"
   }
 }
 
 output "keycloak_client_secret" {
-  value = keycloak_openid_client.openid_client.client_secret
+  value       = keycloak_openid_client.openid_client.client_secret
   description = "Client secret for local development (not sensitive for local env)"
-  sensitive = true
+  sensitive   = true
 }
 
 output "keycloak_issuer" {
-  value = "http://localhost:9003/realms/${keycloak_realm.this.realm}"
+  value       = "http://localhost:9003/realms/${keycloak_realm.this.realm}"
   description = "Keycloak issuer URL"
 }
 
 output "keycloak_client_id" {
-  value = keycloak_openid_client.openid_client.client_id
+  value       = keycloak_openid_client.openid_client.client_id
   description = "Keycloak client ID"
 }
 
 output "admin_user_id" {
-  value = keycloak_user.admin_user.id
+  value       = keycloak_user.admin_user.id
   description = "Admin user UUID (used in ADMIN_USER_ID env var)"
 }
 
 output "dev_user_id" {
-  value = keycloak_user.user_with_initial_password.id
+  value       = keycloak_user.user_with_initial_password.id
   description = "Dev user UUID (alice)"
 }
